2278d22686919abc88ab903e88e4986b1166dd8e
marcos Morais <marcos.cesarmorais@gmail.com>
2022-06-03 13:24:32 -0300

    Criado operacao de instaciacao de categoria

--- src/main/java/com/mcm/sp/SistemaDePedidoApplication.java
+++ src/main/java/com/mcm/sp/SistemaDePedidoApplication.java
@@ -1,13 +1,35 @@
 package com.mcm.sp;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SistemaDePedidoApplication {
+import com.mcm.sp.entities.Categoria;
+import com.mcm.sp.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SistemaDePedidoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaDePedidoApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
+		
 	}
 
 }
