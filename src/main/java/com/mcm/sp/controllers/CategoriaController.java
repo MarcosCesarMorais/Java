ca765ee497f324f34b654abaae3755ad27603346
marcos Morais <marcos.cesarmorais@gmail.com>
2022-06-03 11:14:25 -0300

    Criado repository e service para Categoria

--- src/main/java/com/mcm/sp/controllers/CategoriaController.java
+++ src/main/java/com/mcm/sp/controllers/CategoriaController.java
@@ -3,17 +3,23 @@ package com.mcm.sp.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mcm.sp.entities.Categoria;
+import com.mcm.sp.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
 	
-
+	@Autowired
+	private CategoriaService categoriaService;
+	
 	@GetMapping
 	public List<Categoria> listar(){ 
 		
@@ -26,5 +32,13 @@ public class CategoriaController {
 		
 		
 		return lista;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> listaPorId(@PathVariable Integer id){ 
+			
+		Categoria obj = categoriaService.buscar(id);
+				
+		return ResponseEntity.ok().body(obj);
 	}
 }
