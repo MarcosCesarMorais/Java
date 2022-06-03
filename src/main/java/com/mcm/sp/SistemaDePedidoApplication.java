a4f99e2f09047da3b04a8e4d86c610bb6fdc0bf4
marcos Morais <marcos.cesarmorais@gmail.com>
2022-06-03 09:09:54 -0300

    Projeto Criado

---
+++ src/main/java/com/mcm/sp/SistemaDePedidoApplication.java
@@ -0,0 +1,13 @@
+package com.mcm.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDePedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDePedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcm/sp/SistemaDePedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcm.sp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDePedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
